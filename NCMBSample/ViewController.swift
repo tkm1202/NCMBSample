993d4a9e935f08834e72b41d5fd49df41ed1623c
tkm1202 <tkm1202@users.noreply.github.com>
2020-05-19 00:48:35 +0900

    Initial Commit

---
+++ NCMBSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCMBSample
+//
+//  Created by 加藤拓洋 on 2020/05/19.
+//  Copyright © 2020 TakumiKato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
